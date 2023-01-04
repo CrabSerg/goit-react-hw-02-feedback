a15dd58a4beecf59488054395ede27c466da4479
Serg <serg212007@gmail.com>
2023-01-04 11:05:16 +0200

    Update Section.styled.js

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const Title = styled.h2`
-  font-size: 32px;
+  font-size: 40px;
   font-weight: 700;
   text-align: center;
-  color: black;
+  color: #6ec225;
   margin-bottom: 20px;
 `;
